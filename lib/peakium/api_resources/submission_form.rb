dad6084c2c1a42de9eaf6e2904ad7adecee5e67e
Dan Schultzer <dan@dreamconception.com>
2013-08-15 13:38:26 -0500

    Bug fix build method for SubmissionForm resource

--- lib/peakium/api_resources/submission_form.rb
+++ lib/peakium/api_resources/submission_form.rb
@@ -1,7 +1,7 @@
 module Peakium
   class SubmissionForm < APIResource
-    def self.build(type, api_key=nil)
-      response, api_key = Peakium.request(:post, self.endpoint_url + '/' + type, api_key)
+    def self.build(type, params, api_key=nil)
+      response, api_key = Peakium.request(:post, self.endpoint_url + '/' + type, api_key, params)
       Util.convert_to_peakium_object(response, api_key)
     end
   end
